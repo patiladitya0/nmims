8af6cea6dba623f2b49d15c3467ba2903efaee51
patiladitya0 <aditya03105@gmail.com>
2024-09-26 18:33:14 +0530

    dada

---
+++ src/page/search/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => {
+    
+    return (
+        <div>
+            <h1>Search Page</h1>
+            <p>Search for items here!</p>
+        </div>
+    );
+};
+
+export default Search;
