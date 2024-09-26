ed192fc026c2dffa45c0e8e2791d2f2f6aaf69c3
patiladitya0 <aditya03105@gmail.com>
2024-09-26 19:02:27 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/menu" element={<Menu />} /> {/* Correctly nested route */}
-      </Route>
+     
     </Routes>
   );
 };
