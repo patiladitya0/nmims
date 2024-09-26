b9996434e09f22a4548935547805451a128b9048
patiladitya0 <aditya03105@gmail.com>
2024-09-26 15:22:21 +0530

    Entery auth done 26/9_3:22

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import GetStart from './Components/getstarted/getstart';  // Correct path
-import Option from './Components/getstarted/option';      // Correct path
+import GetStart from './Components/getstarted/getstart';  
+import Option from './Components/getstarted/option';      
+import Login from './Components/auth/login';              
+import Signup from './Components/auth/signup';            
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<GetStart />} />
       <Route path="/second" element={<Option />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 };
