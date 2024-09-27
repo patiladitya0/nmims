630e076586132fae75c22fd45065334bbc109855
patiladitya0 <aditya03105@gmail.com>
2024-09-27 14:52:24 +0530

    added home page

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './home.css'; // Importing the CSS file
+import modulesData from './modules.json'; // Importing the JSON data
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the Home page!</p>
+        <div className="home-container">
+            {/* Title Section */}
+            <div className="title-section">
+                <h1>This is Our Modules</h1>
+            </div>
+
+            {/* Scrollable Module Section */}
+            <div className="module-section">
+                <div className="module-grid">
+                    {modulesData.map((module, index) => (
+                        <Link to={module.link} className="module" key={index}>
+                            <div className="module-content">
+                                <i className={module.icon}></i>
+                                <span>{module.name}</span>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+
+            {/* Additional Content Section */}
+            <div className="additional-content">
+                <p>Explore more features and updates.</p>
+            </div>
         </div>
     );
 };
