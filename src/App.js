c8d629f92812cd1e685e6292307e7bc6680b0288
patiladitya0 <101057378+patiladitya0@users.noreply.github.com>
2024-11-09 22:59:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
         <Route path="myactivity" element={<ProtectedRoute><Myactivity /></ProtectedRoute>} />
         <Route index element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+        <Route path="personalinfo" element={<ProtectedRoute><PersonalInfo /></ProtectedRoute>} />
         <Route path="search" element={<ProtectedRoute><Search /></ProtectedRoute>} />
         <Route path="menu" element={<ProtectedRoute><Menu /></ProtectedRoute>} /> 
         <Route path="maps" element={<ProtectedRoute><Maps /></ProtectedRoute>} /> 
